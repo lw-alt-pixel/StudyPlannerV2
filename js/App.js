2155aea4b9e7aa05d0fa5721e1def75284bf5109
lw-alt-pixel <luke.wong.1120@gmail.com>
2026-04-16 21:04:56 +0800

    Update App.js

--- js/App.js
+++ js/App.js
@@ -2,16 +2,13 @@
 import { store } from './State.js';
 import { uiManager } from './UIManager.js';
 import { timerUI } from './TimerUI.js';
-import { themeManager } from './ThemeManager.js'; // <-- NEW
+import { themeManager } from './ThemeManager.js';
+import { canvasUI } from './CanvasUI.js'; // <-- NEW
 
 console.log("🚀 App.js is connected!");
 
 // Start the Managers
 uiManager.init();
 timerUI.init();
-themeManager.init(); // <-- NEW: Start the Stylist
-
-// Let's test the ThemeManager by changing the color after 2 seconds!
-setTimeout(() => {
-    store.update('theme', t => ({ ...t, appBgColor: '#e0e7ff' })); // Changes to a soft indigo
-}, 2000);
+themeManager.init();
+canvasUI.init(); // <-- NEW: Start the Infinite Canvas
