691ca44866f4ca5e1dc560a49684aec9cd87b931
lw-alt-pixel <luke.wong.1120@gmail.com>
2026-04-16 20:46:15 +0800

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,8 +1,8 @@
 // js/App.js
 import { store } from './State.js';
+import { uiManager } from './UIManager.js';
 
 console.log("🚀 App.js is connected!");
-console.log("🧠 Checking the Brain (State.js):", store.state);
 
-// Let's test our update system!
-store.update('activeTab', () => 'pomodoro');
+// Start the UI Manager
+uiManager.init();
