ee6b28b1c1d01575dc9ff8ef8872c06916375a13
lw-alt-pixel <luke.wong.1120@gmail.com>
2026-04-16 20:43:16 +0800

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,1 +1,8 @@
-console.log("🚀 App.js is connected and working!");
+// js/App.js
+import { store } from './State.js';
+
+console.log("🚀 App.js is connected!");
+console.log("🧠 Checking the Brain (State.js):", store.state);
+
+// Let's test our update system!
+store.update('activeTab', () => 'pomodoro');
