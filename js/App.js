79551bb276322d61614f9906fead481df17da06d
lw-alt-pixel <luke.wong.1120@gmail.com>
2026-04-17 18:25:46 +0800

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,16 +1,16 @@
-// js/App.js
 import { store } from './State.js';
 import { uiManager } from './UIManager.js';
 import { timerUI } from './TimerUI.js';
 import { themeManager } from './ThemeManager.js';
 import { canvasUI } from './CanvasUI.js';
-import { blockManager } from './BlockManager.js'; // <-- NEW
+import { blockManager } from './BlockManager.js'; 
+import { statsUI } from './StatsUI.js'; 
 
-console.log("🚀 App.js is connected!");
-
-// Start the Managers
-uiManager.init();
-timerUI.init();
-themeManager.init();
-canvasUI.init();
-blockManager.init(); // <-- NEW: Start the Block Manager
+document.addEventListener('DOMContentLoaded', () => {
+    uiManager.init();
+    timerUI.init();
+    themeManager.init();
+    canvasUI.init();
+    blockManager.init(); 
+    statsUI.init(); 
+});
