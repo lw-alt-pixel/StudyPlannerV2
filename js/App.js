361ee5048cfd65df75496dadfd51c1ef62047331
lw-alt-pixel <luke.wong.1120@gmail.com>
2026-04-17 23:33:37 +0800

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,3 +1,4 @@
+// js/App.js
 import { store } from './State.js';
 import { uiManager } from './UIManager.js';
 import { timerUI } from './TimerUI.js';
@@ -6,6 +7,8 @@ import { canvasUI } from './CanvasUI.js';
 import { blockManager } from './BlockManager.js'; 
 import { statsUI } from './StatsUI.js'; 
 import { examManager } from './ExamManager.js';
+import { settingsManager } from './SettingsManager.js'; // NEW
+import { floatingWidgetManager } from './FloatingWidgetManager.js'; // NEW
 
 document.addEventListener('DOMContentLoaded', () => {
     uiManager.init();
@@ -15,4 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     blockManager.init(); 
     statsUI.init(); 
     examManager.init();
+    settingsManager.init(); // NEW
+    floatingWidgetManager.init(); // NEW
 });
